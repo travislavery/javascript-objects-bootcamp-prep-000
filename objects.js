0f4fc2ea9d5871abd01860ea7774baae34c267b0
Travis Lavery <travisalavery@gmail.com>
2017-11-21 01:55:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,10 @@ var playlist = {
 }
 
 function updatePlaylist(playlistObj, artistName, songTitle) {
-  Object.assign(playlistObj, )
+  Object.assign(playlistObj, {artistName: songTitle})
+  return playlistObj
+}
+
+function removeFromPlaylist(playlistObj, artistName) {
+  delete playlistObj.artistName
 }
